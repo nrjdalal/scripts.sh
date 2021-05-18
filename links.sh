046d8730ca9ca66c4b7ef0bea4e73e910afa51ec
Neeraj Dalal <nd941z@gmail.com>
2021-05-18 17:32:45 +0530

    Tue May 18 12:02:45 UTC 2021

---
+++ links.sh
@@ -0,0 +1,2 @@
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/nrjdalal/scripts.sh/master/adduser.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/nrjdalal/scripts.sh/master/adduser.sh)"
