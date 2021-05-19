3f3186f5aac91ebc14a630ba078fff78f3acd39a
Neeraj Dalal <nd941z@gmail.com>
2021-05-19 22:47:29 +0530

    Wed May 19 17:17:29 UTC 2021

--- runner.d.sh
+++ runner.d.sh
@@ -2,19 +2,22 @@
 
 set -e
 
+echo
 read -p "enter the project name: " PROJECT
 
 mkdir -p /var/www/$PROJECT.git/dev
 cd /var/www/$PROJECT.git
 
-curl -o actions-runner-linux-x64-2.278.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.278.0/actions-runner-linux-x64-2.278.0.tar.gz
+curl -os actions-runner-linux-x64-2.278.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.278.0/actions-runner-linux-x64-2.278.0.tar.gz
 
 tar xzf ./actions-runner-linux-x64-2.278.0.tar.gz
 
 rm ./actions-runner-linux-x64-2.278.0.tar.gz
 
+echo
 read -p "would you like to configure the runner: " ASK
 
 if [[ $ASK == 'y' || $ASK == 'Y' ]]; then
-  ./configure.sh
+  echo
+  ./config.sh
 fi
