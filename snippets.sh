27ed506251806ccf23705a086a33fd25db89971b
Neeraj Dalal <nd941z@gmail.com>
2021-05-24 19:21:04 +0530

    Update snippets.sh

--- snippets.sh
+++ snippets.sh
@@ -1,2 +1,2 @@
-# updating a specfic line with desired content
+# updating a specfic line with desired content, e.g. Version="datetime-stamp"
 sed -i '' "${lineNumber}s/.*/Version=\"$(date -u +'%y%m%d-%H%M%S')\"/" ${directory}
