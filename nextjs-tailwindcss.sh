190435d14379f128fcf02b7df157ed4d1cd83fba
Neeraj Dalal <nd941z@gmail.com>
2021-10-07 06:19:14 +0530

    Update nextjs-tailwindcss.sh

--- nextjs-tailwindcss.sh
+++ nextjs-tailwindcss.sh
@@ -1,8 +1,25 @@
 yarn create next-app -e with-tailwindcss .
+yarn add @tailwindcss/typography @tailwindcss/forms @tailwindcss/line-clamp @tailwindcss/aspect-ratio
 
 echo -e "\n# lock files\npackage-lock.json\nyarn.lock" >>.gitignore
 
 mkdir -p styles
 echo -e "@tailwind base;\n@tailwind components;\n@tailwind utilities;" >>styles/global.css
 
-sed -i '' 's#tailwindcss/tailwind.css#../styles/globals.css#g' pages/_app.js
+sed -i '' 's#tailwindcss/tailwind.css#styles/globals.css#g' pages/_app.js
+
+cat >>jsconfig.json <<CREATE
+{
+  "compilerOptions": {
+    "baseUrl": "."
+  }
+}
+CREATE
+
+cat >>next.config.js <<CREATE
+module.exports = {
+  reactStrictMode: true,
+}
+CREATE
+
+yarn dev
