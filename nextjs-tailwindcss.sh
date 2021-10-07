a44f48830d6317361432d5271afbd3cdad681f69
Neeraj Dalal <nd941z@gmail.com>
2021-10-07 05:37:17 +0530

    Update nextjs-tailwindcss.sh

--- nextjs-tailwindcss.sh
+++ nextjs-tailwindcss.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-
 yarn create next-app -e with-tailwindcss .
 
-if grep -Fxq "yarn.lock" .gitignore; then
-else
-  echo "yarn.lock" >>.gitignore
-fi
+echo -e "\n# lock files\npackage-lock.json\nyarn.lock" >>.gitignore
+
+mkdir -p styles
+echo -e "@tailwind base;\n@tailwind components;\n@tailwind utilities;" >>styles/global.css
+
+sed -i '' 's#tailwindcss/tailwind.css#../styles/globals.css#g' pages/_app.js
