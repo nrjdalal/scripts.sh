22bcf21380a72cc322c35ec963b3826693a353d6
Neeraj Dalal <nd941z@gmail.com>
2022-03-11 12:36:10 +0530

    Update neur.sh

--- neur.sh
+++ neur.sh
@@ -1,1 +1,35 @@
+# Primary clean up
+rm -rf ../neur-sam
+rm -rf ../neur-sam-no-git
 
+# Copy neur to neur-sam-no-git
+cp -r $(pwd) ../neur-sam-no-git
+
+# Remove .git from neur-sam-no-git
+rm -rf ../neur-sam-no-git/.git
+
+cd ..
+
+# Cloning neur-sam and switching to it
+git clone https://github.com/nrjdalal/neur-sam.git
+cd ./neur-sam
+
+# Doing mandatory cleanup
+rm -rf *
+rm -rf ./.vscode
+rm -rf ./.prettierrc.json
+
+# Moving files from neur-sam-no-git to neur-sam
+mv ../neur-sam-no-git/* ../neur-sam
+mv ../neur-sam-no-git/.* ../neur-sam
+
+# Auto commit
+git add .
+git commit -m "neur-sam"
+git push origin main
+
+cd ..
+
+# Secondary clean up
+rm -rf ./neur-sam
+rm -rf ./neur-sam-no-git
