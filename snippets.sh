dfc02314f06eba73c963979fbac835862941c01b
Neeraj Dalal <nd941z@gmail.com>
2022-04-11 14:40:18 +0530

    Update snippets.sh

--- snippets.sh
+++ snippets.sh
@@ -1,2 +1,5 @@
 # updating a specfic line with desired content, e.g. Version="datetime-stamp"
 sed -i '' "${lineNumber}s/.*/Version=\"$(date -u +'%y%m%d-%H%M%S')\"/" ${directory}
+
+# getting dirs excluding 3 latest
+ls -1 | grep -v "$(ls -1 | tail -n 3)"
